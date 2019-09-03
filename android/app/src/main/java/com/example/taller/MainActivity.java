f200b49eabf916e26c84d6ddfb2236fe99cf685c
MaycolCuero <maicolcr97@hotmail.com>
2019-09-03 14:00:22 -0500

    se definieron las varibales que obtienen los valores de los campos correo y clave

--- android/app/src/main/java/com/example/taller/MainActivity.java
+++ android/app/src/main/java/com/example/taller/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.taller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String email = "", pass = "";
+        EditText c = findViewById(R.id.correo);
+        EditText cl = findViewById(R.id.clave);
+        email = c.getText().toString();
+        pass = cl.getText().toString();
+
+        if(email.isEmpty() && pass.isEmpty()){
+
+        }
     }
+
+
 }
