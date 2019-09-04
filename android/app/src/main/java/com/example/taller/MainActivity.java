37e20fca00dd9ed7db115298523407741c526c97
MaycolCuero <maicolcr97@hotmail.com>
2019-09-03 22:51:23 -0500

    validacion de campos vacios agregada2

--- android/app/src/main/java/com/example/taller/MainActivity.java
+++ android/app/src/main/java/com/example/taller/MainActivity.java
@@ -3,23 +3,35 @@ package com.example.taller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+        EditText c, cl;
+        String email = "", pass = "";
+        
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        c = findViewById(R.id.correo);
+        cl = findViewById(R.id.clave);
+       
+    }
 
-        String email = "", pass = "";
-        EditText c = findViewById(R.id.correo);
-        EditText cl = findViewById(R.id.clave);
+    public void validar(View view){
         email = c.getText().toString();
         pass = cl.getText().toString();
 
         if(email.isEmpty() && pass.isEmpty()){
-
+            Toast.makeText(this, "Los campos estan vacios", Toast.LENGTH_SHORT).show();
+        }else if(email != pass){
+            Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Login Ok", Toast.LENGTH_SHORT).show();
         }
     }
 
