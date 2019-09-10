c0dbe4cff78b01317c33daf81dbf34f66e1727f3
MaycolCuero <maicolcr97@hotmail.com>
2019-09-10 16:30:32 -0500

    menus competados

--- Login/app/src/main/java/com/Maycol/login/Menu.java
+++ Login/app/src/main/java/com/Maycol/login/Menu.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -23,9 +24,37 @@ public class Menu extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.colors2, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Cash", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Yellow", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Blue", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Red", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Subitem5:
+                Toast.makeText(this, "Brown", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Subitem6:
+                Toast.makeText(this, "Green", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+    }
 
     // muestra el menu cuando se preciona
     @Override
