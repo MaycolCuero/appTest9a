e38c853947ee652633a379510c1b9a7691eb13ec
MaycolCuero <maicolcr97@hotmail.com>
2019-09-17 15:44:17 -0500

    se agrego auna funcionalidad a onProgress

--- Colors/app/src/main/java/com/example/colors/MainActivity.java
+++ Colors/app/src/main/java/com/example/colors/MainActivity.java
@@ -1,23 +1,62 @@
 package com.example.colors;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+    private SeekBar sbrRed = null;
+    private SeekBar sbrGreen = null;
+    private SeekBar sbrBlue = null;
+    private SeekBar sbrAlpha = null;
+    private View viewColors = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        private SeekBar sbrRed = null;
-        private SeekBar sbrGreen = null;
-        private SeekBar sbrBlue = null;
-        private SeekBar sbrAlpha = null;
-        private View viewColors = null;
+        //obtenemos los datos
+        sbrRed = findViewById(R.id.sbrRed);
+        sbrGreen = findViewById(R.id.sbrGreen);
+        sbrBlue = findViewById(R.id.sbrBlue);
+        sbrAlpha = findViewById(R.id.sbrAlpha);
+
+        sbrRed.setOnSeekBarChangeListener(this);
+        sbrGreen.setOnSeekBarChangeListener(this);
+        sbrBlue.setOnSeekBarChangeListener(this);
+        sbrAlpha.setOnSeekBarChangeListener(this);
+
+        // muestra o aplica el menu contextual cuando se mantiene precionado viewColors, permite
+        // aplicar un menu contextual a cualquier view en este caso a viewcolors
+        registerForContextMenu(viewColors);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        int r = sbrRed.getProgress();
+        int g = sbrGreen.getProgress();
+        int bl = sbrBlue.getProgress();
+        int a = sbrAlpha.getProgress();
+
+        int color = Color.argb(a,r,g,bl);
+        viewColors.setBackgroundColor(color);
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
