a1ffd41ee54cddca67b3490904511f9b3cd4c72d
MaycolCuero <maicolcr97@hotmail.com>
2019-10-02 18:13:58 -0500

    guardado de datos en db aun no completo

--- Login/app/src/main/java/com/Maycol/login/SignUp.java
+++ Login/app/src/main/java/com/Maycol/login/SignUp.java
@@ -2,17 +2,27 @@ package com.Maycol.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.Maycol.login.classes.connectionDB;
 
 public class SignUp extends AppCompatActivity {
-
+    EditText fname;
+    EditText lname;
+    EditText emaiñ;
+    EditText pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        fname = findViewById(R.id.txtIdFirstName);
 
         Button btn2 = (Button) findViewById(R.id.btnRegister);
         btn2.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +32,32 @@ public class SignUp extends AppCompatActivity {
                 startActivityForResult(intent2, 0);
             }
         });
+
+
+
+    }
+
+    public void Signup(View view){
+        //cerate the conneciton assistent
+        connectionDB manager = new connectionDB(this, "data",null,1);
+        //Let read/write on DB
+        SQLiteDatabase market = manager.getWritableDatabase();
+        //Get values
+        String FName = fname.getText().toString();
+        String LName = "";
+        String Email = "";
+        String Pass = "";
+
+        ContentValues DATA = new ContentValues();
+
+        DATA.put("firstname",FName);
+        DATA.put("lastname",LName);
+        DATA.put("email",Email);
+        DATA.put("pass", Pass);
+
+        //Save values into DB
+        market.insert("users", null,DATA);
+        Toast.makeText(this,"The user", Toast.LENGTH_LONG);
+        market.close();
     }
 }
