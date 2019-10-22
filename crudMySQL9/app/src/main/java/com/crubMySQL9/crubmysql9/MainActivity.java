77ee8de6453c74e7b279bdf91012f8ea7be20dd8
MaycolCuero <maicolcr97@hotmail.com>
2019-10-22 16:23:54 -0500

    avanzado

--- crudMySQL9/app/src/main/java/com/crubMySQL9/crubmysql9/MainActivity.java
+++ crudMySQL9/app/src/main/java/com/crubMySQL9/crubmysql9/MainActivity.java
@@ -3,12 +3,19 @@ package com.crubMySQL9.crubmysql9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText email;
+    Button btn_Reguser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.iduseremail);
+        btn_Reguser = findViewById(R.id.idRegister);
+
     }
 }
